b3e9e7f4a3d0390d5f12ca16f8a786df63e5837c
DEVINDHI\ACER <devindig@treinetic.com>
2026-03-11 19:57:08 +0530

    updated cors

--- src/app/api.py
+++ src/app/api.py
@@ -29,6 +29,9 @@ app.add_middleware(
         "http://localhost:3000",  # Alternative dev port
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "http://ikms-api.servecounterstrike.com",
+        "https://ikms-api.servecounterstrike.com",
+        "https://ikms-frontend-jeyv.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
